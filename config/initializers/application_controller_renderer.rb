9c02e9e41996308a4a3c44b6d64a1f3f56a8707b
EC2 Default User <ec2-user@ip-172-31-16-150.ap-northeast-1.compute.internal>
2020-11-28 17:09:06 +0000

    refile

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,4 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+Refile.secret_key = '552ba085a9057edcff29c3daf6080d11073dcd63926873438bb5ba04daa36c48d08209f3c6cda89b152a4229d95a4ebddaced276dc275577576f2f2fa8aaba54'
